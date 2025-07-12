88d19421c81253edfbf40625bc3c172b914cf9f1
Vansh Jindal <vansh.jindal.2016@gmail.com>
2025-07-12 23:45:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("start").addEventListener("click", () => {
+  alert("Start exploring button clicked!");
+});
+document.getElementById("view-all").addEventListener("click", () => {
+  location.href = "#"; // link to listing page
+});
